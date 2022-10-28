70a0ff9575805474ab190d0e8669e59da146a220
Angel <santoyox714@gmail.com>
2022-10-28 13:28:19 -0700

    more signatures!

--- src/lib/pages/signatures.ts
+++ src/lib/pages/signatures.ts
@@ -14,7 +14,7 @@
         image: 'potato.png'
     },
     {
-        name: 'Jacob',
+        name: 'Jacob C',
         message: "",
         message2: "" ,
         link: '',
@@ -26,6 +26,55 @@
         message2: "🐒" ,
         link: '',
         image: 'smile.png'
+    },
+    {
+        name: 'Deanna',
+        message: "Happy Birthday stinky",
+        message2: "" ,
+        link: '',
+        image: 'smile.png'
+    },
+    {
+        name: 'Christos',
+        message: "happy birthday.",
+        message2: "" ,
+        link: '',
+        image: 'frog.png'
+    },
+    {
+        name: 'Yao',
+        message: "happy birthday charlie. take it easy and hope you have a great one        ",
+        message2: "I hope you have a miraculously monkey birthday enjoy this goated MonkE cranking 90s" ,
+        link: '',
+        image: 'anime.png'
+    },
+    {
+        name: 'Alan',
+        message: "Happy Birthday Monke Man",
+        message2: "I hope you have a miraculously monkey birthday enjoy this goated MonkE cranking 90s" ,
+        link: '',
+        image: 'monkey2.gif'
+    },
+    {
+        name: 'Jacob N',
+        message: "Happy 22nd Birthday Charlie. You found a shiny monke!        ",
+        message2: "" ,
+        link: '',
+        image: 'chimchar.png'
+    },
+    {
+        name: 'Aaron',
+        message: "",
+        message2: "" ,
+        link: '',
+        image: 'coding.gif'
+    },
+    {
+        name: 'Steph',
+        message: "Happy birthday Monkey Man!! ",
+        message2: "Being your friend has been super fun and I'm glad we've shared these good times together! Have fun this weekend and drink plenty of mint mojitos for me. kys" ,
+        link: '',
+        image: 'snapchat.png'
     },
    
 ]
